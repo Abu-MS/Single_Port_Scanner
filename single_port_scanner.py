02d0275a0e5f89f77b57952f533b70808a1c76a4
Abu-MS <abushahilsiraj@gmail.com>
2025-04-12 20:20:50 +0530

    __init__ added

--- single_port_scanner.py
+++ single_port_scanner.py
@@ -5,11 +5,14 @@ import socket
 s = socket
 
 def dns_check(domain):
-    """DNS resolution check is done i.e. the domain name given by the user is converted into ipv4 address.
+    """
+    DNS resolution check is done i.e. the domain name given by the user is converted into ipv4 address.
     If successfully resolved returns the ipv4 address.
-    If resolution failed, it raises a socket.gaierror i.e, invalid domain or no DNS record which is caught and returns None value."""
+    If resolution failed, it raises a socket.gaierror i.e, invalid domain or no DNS record which is caught and returns None value.
+    """
 
     try:
+
         IP = s.gethostbyname(domain)  # Successful DNS Resolution returns the IPv4 address.
         print(f'Domain IP is: {IP}')
         return IP
@@ -18,9 +21,11 @@ def dns_check(domain):
         return None
 
 def port_check(p):
-    """This function checks whether the port given by the user falls in between valid TCP/UDP port range (0-65535).
+    """
+    This function checks whether the port given by the user falls in between valid TCP/UDP port range (0-65535).
     If true returns the port number.
-    If false, then prints an error message indicating the port is out of range."""
+    If false, then prints an error message indicating the port is out of range.
+    """
 
     if p <0 or p > 65535:  # Port range check if out of range, notifies user.
         print(f'Port {p} is out of range.')
@@ -28,11 +33,13 @@ def port_check(p):
         return p  # If port check is valid returns the port.
 
 def single_port_scanner(IP, port):
-    """This function attempts to connect to the IP and port using TCP connection.
+    """
+    This function attempts to connect to the IP and port using TCP connection.
     If the port is Open, success message is print.
     If not it catches the error and notifies the user.
     The socket has a timeout of 5 seconds (set by settimeout(5)).
-    This means the socket will wait up to 5 seconds for a response before raising a timeout error."""
+    This means the socket will wait up to 5 seconds for a response before raising a timeout error.
+    """
 
     # Establishing a connection, (#IPv4 add, TCP connection)
     c = s.socket(s.AF_INET, s.SOCK_STREAM)
@@ -48,14 +55,15 @@ def single_port_scanner(IP, port):
     finally:
         c.close()  # closes the socket connection after the scan.
 
+if __name__ == '__main__':
 
-domain_to_IP = dns_check(input('Enter domain: '))
+    domain_to_IP = dns_check(input('Enter domain: '))
+    print("Running")
+    # The port scan is done after both the dns check and port check are successful.
+    if domain_to_IP:  # If DNS resolution is successful proceeds to port check.
+        user_port = port_check(int(input('Enter port: ')))
 
-# The port scan is done after both the dns check and port check are successful.
-if domain_to_IP:  # If DNS resolution is successful proceeds to port check.
-    user_port = port_check(int(input('Enter port: ')))
-
-    if user_port is not None:  # if port check is successful proceeds to port scan.
-        single_port_scanner(domain_to_IP, user_port)
-else:
-    print('Domain failed to resolve.')
+        if user_port is not None:  # if port check is successful proceeds to port scan.
+            single_port_scanner(domain_to_IP, user_port)
+    else:
+        print('Domain failed to resolve.')
